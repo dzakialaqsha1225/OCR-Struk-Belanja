178d8f62c60ca7970bd4129be1e74bca358eb393
dzakialaqsha <dzakialaqsha1225@gmail.com>
2024-12-11 04:03:45 +0700

    Update full_deployment.py

--- full_deployment.py
+++ full_deployment.py
@@ -5,7 +5,7 @@ import vertex_extract_dict as ved
 import product_recommender as pr
 import cheap_close as cc
 
-def full_deployment(key_path: str, test_path: str, uid: str, email: str, model, lon: float, lat: float):
+def full_deployment(key_path: str, test_path: str, dataset_path: string, uid: str, email: str, model, lon: float, lat: float):
   """
     Takes a picture of a receipt, perform object localization for the receipt, use ocr on cropped localized image,
     then generate recommended places to get similar items for cheaper and closer.
@@ -15,6 +15,7 @@ def full_deployment(key_path: str, test_path: str, uid: str, email: str, model, 
     Args:
       key_path (str): Path to the Google Cloud service account JSON key file.
       test_path (str): Path to the image file of the receipt.
+      dataset_path(str): Path to the purchase history dataset.
       uid (str): User ID.
       email (str): User email address.
       model (Any): The object detection model to be used for receipt localization.
@@ -25,6 +26,17 @@ def full_deployment(key_path: str, test_path: str, uid: str, email: str, model, 
       a pd dataframe sorted by distance from user's location, offering the cheapest price, at the most up-to-date
       of user's previously purchased items and recommended items based on RFM analysis
   """
+  if pd.read_csv(dataset_path).empty:
+  raise ValueError("DataFrame is empty. Please check the dataset file.")
+  if 'uid' not in df.columns:
+    raise ValueError("uid column is missing from the dataset")
+  if re.fullmatch(r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$", email) is None:
+    raise ValueError('Email is not valid')
+  if 'lon' not in pd.read_csv(dataset_path).columns:
+    raise ValueError("long column is missing from the dataset")
+  if 'lat' not in pd.read_csv(dataset_path).columns:
+    raise ValueError("lat column is missing from the dataset")
+  
   warnings.simplefilter(action='ignore', category=FutureWarning)
   struk = ol.ocr_receipt(test_path, model) #uses util
   data = ved.extract_dict(struk, key_path, uid, email) #uses util
